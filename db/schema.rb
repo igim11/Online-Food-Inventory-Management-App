2ddf556776401420296fb0e740bd44dd2b9b44e4
venskie03 <kevinkyleganados03@gmail.com>
2024-01-31 20:01:08 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_30_102228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "orders", force: :cascade do |t|
     t.decimal "total_bill"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
