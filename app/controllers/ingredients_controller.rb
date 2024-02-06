95df09a14550a4ce614c9c3c705dd4a0eb381202
venskie03 <kevinkyleganados03@gmail.com>
2024-02-07 00:36:22 +0800

    adding ingredients to meals

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,11 +1,17 @@
 class IngredientsController < ApplicationController
 
+  def meal_ingredients
+    @list_ingredients = Ingredient.where(meal_id: params[:meal_id])
+    @meals_info = Meal.where(id: params[:meal_id])
+  end
+
   def add
+    meal_ingredients
     @ingredients = Ingredient.new
   end
 
   def create
-    @ingredients = Ingredients.new(ingredients_params)
+    @ingredients = Ingredient.new(ingredients_params)
     if @ingredients.save
       redirect_to add_meal_path
     else
