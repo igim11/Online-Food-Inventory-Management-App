2b196149f37a5a19e9921684ecb82e0d5e2202ec
igim11 <migicastro@yahoo.com>
2024-02-07 11:03:22 +0800

    added total bill to menu

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,24 +15,36 @@ class OrdersController < ApplicationController
     def create
         @order = Order.new(order_params)
         @meals = Meal.all
+        total_bill = calculate_total_bill(params[:order][:order_items_attributes])
+        @order.total_bill = total_bill
 
         @order.order_items = @order.order_items.to_a.reject { |item| item.quantity.zero? }
       
         respond_to do |format|
           if @order.save
-            format.html { redirect_to @order, notice: 'Order was successfully created.' }
+            format.html { redirect_to orders_new_path, notice: 'Order was successfully created.' }
             format.json { render :show, status: :created, location: @order }
           else
             format.html { render :menu, status: :unprocessable_entity }
             format.json { render json: @order.errors, status: :unprocessable_entity }
           end
         end
-    end      
+    end  
 
     private
 
     def order_params
         params.require(:order).permit(:total_bill, :user_id, order_items_attributes: [:meal_id, :order_id, :quantity])
-    end      
+    end
+    
+    def calculate_total_bill(order_items_attributes)
+      total_bill = 0
+      order_items_attributes.each do |index, order_item_params|
+        meal = Meal.find(order_item_params[:meal_id])
+        quantity = order_item_params[:quantity].to_i
+        total_bill += meal.meals_price * quantity
+      end
+      total_bill
+    end
       
 end
