45e6ff99c8b81a8b00ed965b4127c7ae2c85362d
igim11 <migicastro@yahoo.com>
2024-02-16 17:17:54 +0800

    attempt at stimulus and turbo frames and writing test

--- app/javascript/controllers/bill_controller.js
+++ app/javascript/controllers/bill_controller.js
@@ -1,16 +1,21 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["quantity", "mealPrice", "totalCost"];
+
   connect() {
-    const quantity = this.element.value;
-    const mealPrice = parseFloat(this.data.get("mealPrice"));
-    const totalCostElement = document.querySelector(this.data.get("target"));
+    this.calculateTotalCost();
   }
+
+  calculateTotalCost() {
+    const quantity = parseInt(this.quantityTarget.value);
+    const mealPrice = parseFloat(this.mealPriceTarget.textContent);
+    const totalCost = quantity * mealPrice;
+
+    this.totalCostTarget.textContent = `Total Cost: $${totalCost.toFixed(2)}`;
+  }
+
   updateTotalCost() {
-    
-    if (!isNaN(quantity) && !isNaN(mealPrice) && totalCostElement) {
-      const totalCost = mealPrice * quantity;
-      totalCostElement.innerText = totalCost.toFixed(2);
-    }
+    this.calculateTotalCost();
   }
 }
