947d5c42b627feaa4b8a1337af5da91c3a5b8807
venskie03 <kevinkyleganados03@gmail.com>
2024-02-21 13:11:37 +0800

    deploy setup main

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,14 @@
 # exit on error
 set -o errexit
 
+# Install gem dependencies
 bundle install
+
+# Run database migrations
+bundle exec rails db:migrate
+
+# Precompile assets
 bundle exec rails assets:precompile
+
+# Clean up old assets
 bundle exec rails assets:clean
